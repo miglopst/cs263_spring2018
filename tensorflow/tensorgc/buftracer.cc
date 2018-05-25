3357af9177599abf53ec5e77be9ab71cf1ad9cf2
miglopst <hogpe217@gmail.com>
2018-05-25 23:52:30 +0000

    add garbage collection code

--- tensorflow/tensorgc/buftracer.cc
+++ tensorflow/tensorgc/buftracer.cc
@@ -44,12 +44,41 @@ int BufTracer<T>::get_thresh(){
 
 template <typename T>
 void BufTracer<T>::mark_mv_garbage_set(){
-  std::set<T*>::iterator tracing_set_it;
+  typename std::set<T*>::iterator buffer_set_it;
+  typename std::set<T*>::iterator garbage_set_it;
+
   T* temp_buf_ptr;
+  for (buffer_set_it = buffer_set.begin(); buffer_set_it != buffer_set.end(); ++buffer_set_it){
+    temp_buf_ptr = *buffer_set_it;
+    if (tracing_set.find(temp_buf_ptr) == tracing_set.end()){
+      //the given buffer is not in the tracing set!
+      //move it to the garbage_set
+      if(std::getenv("DEBUG_FLAG") && atoi(std::getenv("DEBUG_FLAG")) == 4){
+        std::cout << "[buftracer.cc]: Buffer, bid = " << temp_buf_ptr->getid() << " is moved to garbage_set." << std::endl;
+      }
+      garbage_set.insert(temp_buf_ptr);
+    }
+    else{
+      //the given buffer is in the tracing set!
+      if(std::getenv("DEBUG_FLAG") && atoi(std::getenv("DEBUG_FLAG")) == 4){
+        std::cout << "[buftracer.cc]: Buffer, bid = " << temp_buf_ptr->getid() << " can be traced, not moving to garbage_set." << std::endl;
+      }
+    }
+  }
+  //remove all garbage elements from buffer_set
+  for (garbage_set_it = garbage_set.begin(); garbage_set_it != garbage_set.end(); ++garbage_set_it){
+    temp_buf_ptr = *garbage_set_it;
+    buffer_set.erase(temp_buf_ptr);
+  }
+
+  //[WARNING] remove all elements from tracing_set
+  tracing_set.clear();
 }
 
 template <typename T>
-void BufTracer<T>::free_garbage_set(){}
+void BufTracer<T>::free_garbage_set(){
+  garbage_set.clear();
+}
 
 template class BufTracer<Buffer>;
 }// namespace tensorflow
